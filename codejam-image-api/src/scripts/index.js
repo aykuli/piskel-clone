0d7eb51d2f3ab0c9fc9147af1017ed30529dd0a3
Aykuli <aykuli@ya.ru>
2019-11-19 01:12:36 +0700

    feat: add alert when img not loaded for grayscale

--- codejam-image-api/src/scripts/index.js
+++ codejam-image-api/src/scripts/index.js
@@ -9,13 +9,13 @@ class Picture {
     this.lastY = 0;
     this.currentColor = currentColor;
     this.prevColorCache = '#ffffff';
+
     if (canvas == null) {
       throw new Error('there is no canvas');
     }
   }
 
   saveInLocalStorage(localStorageKey) {
-    console.log('saving in localStorage ', localStorageKey);
     localStorage.removeItem(localStorageKey);
 
     const dataURI = this.canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, '');
@@ -62,10 +62,12 @@ class Picture {
   };
 
   draw = e => {
-    [this.x2, this.y2] = [Math.ceil(e.offsetX / scale), Math.ceil(e.offsetY / scale)];
+    if (this.isDrawing) {
+      [this.x2, this.y2] = [Math.ceil(e.offsetX / scale), Math.ceil(e.offsetY / scale)];
 
-    this.bresenham(this.x1, this.x2, this.y1, this.y2);
-    [this.x1, this.y1] = [this.x2, this.y2];
+      this.bresenham(this.x1, this.x2, this.y1, this.y2);
+      [this.x1, this.y1] = [this.x2, this.y2];
+    }
   };
 
   drawOnMouseDown = e => {
@@ -83,7 +85,6 @@ class Picture {
 
   pencilTool = targetTool => {
     if (targetTool === 'pencil') {
-      console.log(targetTool);
       this.canvas.addEventListener('mousemove', this.draw);
       this.canvas.addEventListener('mousedown', this.drawOnMouseDown);
       this.canvas.addEventListener('mouseup', this.drawMouseUp);
@@ -91,17 +92,15 @@ class Picture {
         this.isDrawing = false;
       });
     } else {
-      console.log(targetTool);
       this.canvas.removeEventListener('mousemove', this.draw);
       this.canvas.removeEventListener('mousedown', this.drawOnMouseDown);
       this.canvas.removeEventListener('mouseup', this.drawMouseUp);
     }
-    this.saveInLocalStorage('piskelCloneImg')
+    this.saveInLocalStorage('piskelCloneImg');
   };
 
   bucketTool(targetTool) {
     if (targetTool === 'bucket') {
-      console.log('bucket tool choosed');
       this.canvas.addEventListener('mousedown', this.floodFill);
     } else {
       this.canvas.removeEventListener('mousedown', this.floodFill);
@@ -109,36 +108,28 @@ class Picture {
   }
 
   floodFill = e => {
-    console.log('inside floodFill');
-
     let [x, y] = [Math.ceil(e.offsetX / scale), Math.ceil(e.offsetY / scale)];
 
     const targetColor = this.RGBToHex(this.ctx.getImageData(x, y, 1, 1).data);
-    let replacementColor = this.currentColor.value;
+    const replacementColor = this.currentColor.value;
     if (targetColor === replacementColor) return;
     this.ctx.fillStyle = replacementColor;
     this.ctx.fillRect(x, y, 1, 1);
     let Queue = [];
 
-    let xy = [x, y];
-    Queue.push(xy);
-    let i = 0;
+    Queue.push([x, y]);
     while (Queue.length > 0) {
-      i++;
-      if (i === 100000) return;
-      xy = [x, y];
-      let xPlus1y = [x + 1, y];
       if (
         x + 1 > 0 &&
         x + 1 < this.canvas.width &&
         targetColor === this.RGBToHex(this.ctx.getImageData(x + 1, y, 1, 1).data)
       ) {
-        Queue.push(xPlus1y);
+        Queue.push([x + 1, y]);
         this.ctx.fillRect(x + 1, y, 1, 1);
       }
 
       if (
-        x - 1 > 0 &&
+        x - 1 >= 0 &&
         x - 1 < this.canvas.width &&
         targetColor === this.RGBToHex(this.ctx.getImageData(x - 1, y, 1, 1).data)
       ) {
@@ -156,7 +147,7 @@ class Picture {
       }
 
       if (
-        y - 1 > 0 &&
+        y - 1 >= 0 &&
         y - 1 < this.canvas.width &&
         targetColor === this.RGBToHex(this.ctx.getImageData(x, y - 1, 1, 1).data)
       ) {
@@ -166,8 +157,7 @@ class Picture {
 
       Queue.shift(0);
       if (Queue.length > 0) {
-        x = Queue[0][0];
-        y = Queue[0][1];
+        [x, y] = [Queue[0][0], Queue[0][1]];
       }
     }
     Queue = [];
@@ -182,7 +172,6 @@ class Picture {
   }
 
   pickerTool(targetTool) {
-    console.log(targetTool);
     if (targetTool === 'picker') {
       this.canvas.addEventListener('click', this.colorPicker);
     } else {
@@ -193,10 +182,10 @@ class Picture {
   RGBToHex(data) {
     let dataHex = '#';
     for (let i = 0; i < 3; i += 1) {
-      let color = data[i];
-      let letter = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-      let firstLetter = letter[Math.floor(color / 16)];
-      let secondLetter = letter[color % 16];
+      const color = data[i];
+      const letter = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+      const firstLetter = letter[Math.floor(color / 16)];
+      const secondLetter = letter[color % 16];
       dataHex = dataHex + firstLetter + secondLetter;
     }
     return dataHex;
@@ -205,8 +194,7 @@ class Picture {
   colorPicker = e => {
     const [x, y] = [Math.ceil(e.offsetX / scale), Math.ceil(e.offsetY / scale)];
     const choosedColor = this.ctx.getImageData(x, y, 1, 1);
-    var data = choosedColor.data;
-    var color = this.RGBToHex(data);
+    const color = this.RGBToHex(choosedColor.data);
 
     this.ctx.fillStyle = color;
     this.currentColor.value = color;
@@ -218,83 +206,78 @@ class Picture {
     this.pickerTool(targetTool);
   };
 
-  windowReload(ctx) {
+  windowReload = () => {
     if (localStorage.getItem('piskelCloneImg') !== null) {
       const img = new Image();
 
       img.onload = () => {
         this.ctx.imageSmoothingEnabled = false;
-        ctx.drawImage(img, 0, 0);
+        this.ctx.drawImage(img, 0, 0);
       };
-      img.src = app.insertFromLocalStorage('piskelCloneImg');
+      img.src = this.insertFromLocalStorage('piskelCloneImg');
     }
 
     if (localStorage.getItem('piskelCloneResolution')) {
       size = +localStorage.getItem('piskelCloneResolution');
-      [canvas.width, canvas.height] = [size, size];
+      [this.canvas.width, this.canvas.height] = [size, size];
 
       const currentRes = document.querySelector('.res-active');
       currentRes.classList.remove('res-active');
-      const target = document.getElementById('res' + size);
+      const target = document.getElementById('res'.concat(size));
       target.classList.add('res-active');
-      if (localStorage.getItem('piskelCloneResolution') === 128) scale = 4;
-      if (localStorage.getItem('piskelCloneResolution') === 256) scale = 2;
+      if (localStorage.getItem('piskelCloneResolution') === '128') scale = 4;
+      if (localStorage.getItem('piskelCloneResolution') === '256') scale = 2;
     }
+  };
 
-    console.log('load piskelCloneResolution =', localStorage.getItem('piskelCloneResolution'));
-  }
-
-  downloadImage(url, canvas) {
-    let img = new Image();
+  downloadImage(url) {
+    const img = new Image();
     img.crossOrigin = 'Anonymous';
     img.src = url;
-    [canvas.width, canvas.height] = [512, 512];
+    [this.canvas.width, this.canvas.height] = [512, 512];
 
-    let [currentWidth, currentHeight] = [canvas.width, canvas.height];
+    let [currentWidth, currentHeight] = [this.canvas.width, this.canvas.height];
     let [x, y] = [0, 0];
     img.onload = () => {
       if (img.naturalWidth > img.naturalHeight) {
-        let scaleImg = img.naturalWidth / canvas.width;
-        currentWidth = canvas.width;
+        const scaleImg = img.naturalWidth / this.canvas.width;
+        currentWidth = this.canvas.width;
         currentHeight = img.naturalHeight / scaleImg;
         x = 0;
-        y = (canvas.height - currentHeight) / 2;
+        y = (this.canvas.height - currentHeight) / 2;
       } else if (img.naturalWidth === img.naturalHeight) {
-        currentWidth = canvas.width;
-        currentHeight = canvas.height;
+        currentWidth = this.canvas.width;
+        currentHeight = this.canvas.height;
       } else {
-        let scaleImg = img.naturalHeight / canvas.height;
+        const scaleImg = img.naturalHeight / this.canvas.height;
         currentWidth = img.naturalWidth / scaleImg;
-        currentHeight = canvas.height;
-        x = (canvas.width - currentWidth) / 2;
+        currentHeight = this.canvas.height;
+        x = (this.canvas.width - currentWidth) / 2;
         y = 0;
       }
       this.ctx.imageSmoothingEnabled = false;
-      ctx.drawImage(img, x, y, currentWidth, currentHeight);
-      app.saveInLocalStorage('piskelCloneImg');
+      this.ctx.drawImage(img, x, y, currentWidth, currentHeight);
+      this.saveInLocalStorage('piskelCloneImg');
     };
   }
 
-  saveCanvas(canvas) {
-    function download(canvas, filename) {
-      // create an "off-screen" anchor tag
-      const lnk = document.createElement('a');
-      // the key here is to set the download attribute of the a tag
-      lnk.download = filename;
-      // convert canvas content to data-uri for link. When download
-      // attribute is set the content pointed to by link will be
-      // pushed as "download" in HTML5 capable browsers
-      lnk.href = canvas.toDataURL('image/png;base64');
-      if (document.createEvent) {
-        const e = document.createEvent('MouseEvents');
-        e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+  saveCanvas() {
+    // create an "off-screen" anchor tag
+    const lnk = document.createElement('a');
+    // the key here is to set the download attribute of the a tag
+    lnk.download = 'myimage.png';
+    // convert canvas content to data-uri for link. When download
+    // attribute is set the content pointed to by link will be
+    // pushed as "download" in HTML5 capable browsers
+    lnk.href = this.canvas.toDataURL('image/png;base64');
+    if (document.createEvent) {
+      const e = document.createEvent('MouseEvents');
+      e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
 
-        lnk.dispatchEvent(e);
-      } else if (lnk.fireEvent) {
-        lnk.fireEvent('onclick');
-      }
+      lnk.dispatchEvent(e);
+    } else if (lnk.fireEvent) {
+      lnk.fireEvent('onclick');
     }
-    download(canvas, 'myimage.png');
   }
 
   highlightCurrentResolution(target) {
@@ -304,27 +287,26 @@ class Picture {
   }
 
   grayscale() {
-    var imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
-    var data = imageData.data;
-    for (var i = 0; i < data.length; i += 4) {
-      var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    const imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    const data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+      const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
       data[i] = avg; // red
       data[i + 1] = avg; // green
       data[i + 2] = avg; // blue
     }
     this.ctx.putImageData(imageData, 0, 0);
   }
-}
 
-function getLinkToImage(city) {
-  let url = `https://api.unsplash.com/photos/random?query=town,${city}&client_id=e2077ad31a806c894c460aec8f81bc2af4d09c4f8104ae3177bb809faf0eac17`;
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data.urls.regular);
-      app.downloadImage(data.urls.regular, canvas);
-    })
-    .catch(err => alert('Put down in input right city'));
+  getLinkToImage(city) {
+    const url = `https://api.unsplash.com/photos/random?query=town,${city}&client_id=e2077ad31a806c894c460aec8f81bc2af4d09c4f8104ae3177bb809faf0eac17`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        this.downloadImage(data.urls.regular);
+      })
+      .catch(() => alert('Put down in input right city'));
+  }
 }
 
 // Start working with index.html
@@ -335,36 +317,22 @@ const currentColor = document.getElementById('currentColor');
 const prevColor = document.querySelector('.color--prev');
 const colorRed = document.querySelector('.color--red');
 const colorBlue = document.querySelector('.color--blue');
-const empty = document.getElementById('empty');
 const prevColorCache = '#ffffff';
 prevColor.children[0].style.background = '#ffffff';
-let dataURI = null;
 let scale = 1;
 let size = 512;
-let cityChoiseInpit = document.getElementById('cityChoiseInpit');
-let city = 'Almaty';
+const cityChoiseInpit = document.getElementById('cityChoiseInpit');
 const load = document.getElementById('load');
 ctx.imageSmoothingEnabled = false;
+let isImgLoaded = false;
+const modalLoadImg = document.querySelector('.modal__no-image');
+const modalOverlay = document.querySelector('.modal__overlay');
 
-const grayScaleBtn = document.getElementById('grayScale');
-
-let app = new Picture(canvas, ctx, currentColor);
-// localStorage.setItem('piskelCloneResolution', size);
-// if (localStorage.getItem('piskelCloneResolution')) {
-//   size = +localStorage.getItem('piskelCloneResolution');
-//   [canvas.width, canvas.height] = [size, size];
-
-//   const currentRes = document.querySelector('.res-active');
-//   currentRes.classList.remove('res-active');
-//   const target = document.getElementById('res' + size);
-//   target.classList.add('res-active');
-//   if (localStorage.getItem('piskelCloneResolution') === 128) scale = 4;
-//   if (localStorage.getItem('piskelCloneResolution') === 256) scale = 2;
-// }
+const app = new Picture(canvas, ctx, currentColor);
 // **********   INITIALIZATION    ************ */
 // Initialization process, loading prev image
 
-window.addEventListener('load', app.windowReload(ctx));
+window.addEventListener('load', app.windowReload());
 
 window.onbeforeunload = () => app.saveInLocalStorage('piskelCloneImg');
 // **********   and of INITIALIZATION    ************ */
@@ -380,15 +348,26 @@ pane.addEventListener('click', e => {
 
   if (targetTool === 'empty') {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    isImgLoaded = false;
     targetTool = 'pencil';
   } else if (targetTool === 'grayscale') {
-    app.grayscale();
+    if (isImgLoaded) {
+      app.grayscale();
+    } else {
+      modalLoadImg.classList.add('display-block');
+      modalOverlay.classList.add('display-block');
+    }
   } else {
     const prevActiveTool = document.querySelector('.tool--active');
     prevActiveTool.classList.remove('tool--active');
     targetToolEl.classList.add('tool--active');
     app.tools(targetTool);
   }
+});
+
+modalOverlay.addEventListener('click', () => {
+  modalLoadImg.classList.remove('display-block');
+  modalOverlay.classList.remove('display-block');
 });
 // ********************    end of TOOLS    *******************/
 
@@ -411,7 +390,7 @@ currentColor.addEventListener('change', app.watchColor(prevColor, currentColor.v
 // ********************    SAVE IMAGE    *********************/
 const save = document.getElementById('save');
 save.addEventListener('click', () => {
-  app.saveCanvas(canvas);
+  app.saveCanvas();
 });
 // *****************  end of  SAVE IMAGE    ******************/
 
@@ -452,7 +431,7 @@ canvasResolution.addEventListener('click', e => {
       scale = 2;
       break;
 
-    case 'res512':
+    default:
       size = 512;
       scale = 1;
       break;
@@ -463,7 +442,7 @@ canvasResolution.addEventListener('click', e => {
   canvas.height = size;
   app.highlightCurrentResolution(e.target);
 
-  let img = new Image();
+  const img = new Image();
   img.onload = () => {
     this.ctx.imageSmoothingEnabled = false;
     ctx.drawImage(img, 0, 0);
@@ -471,9 +450,9 @@ canvasResolution.addEventListener('click', e => {
   img.src = app.insertFromLocalStorage('piskelCloneImg');
   let [currentWidth, currentHeight] = [canvas.width, canvas.height];
   let [x, y] = [0, 0];
-  img.onload = function () {
+  img.onload = () => {
     if (img.naturalWidth > img.naturalHeight) {
-      let scaleImg = img.naturalWidth / canvas.width;
+      const scaleImg = img.naturalWidth / canvas.width;
       currentWidth = canvas.width;
       currentHeight = img.naturalHeight / scaleImg;
       x = 0;
@@ -482,7 +461,7 @@ canvasResolution.addEventListener('click', e => {
       currentWidth = canvas.width;
       currentHeight = canvas.height;
     } else {
-      let scaleImg = img.naturalHeight / canvas.height;
+      const scaleImg = img.naturalHeight / canvas.height;
       currentWidth = img.naturalWidth / scaleImg;
       currentHeight = canvas.height;
       x = (canvas.width - currentWidth) / 2;
@@ -495,16 +474,15 @@ canvasResolution.addEventListener('click', e => {
 });
 
 load.addEventListener('click', () => {
-  city = cityChoiseInpit.value;
+  const city = cityChoiseInpit.value;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  console.log(city);
-  // getLinkToImage(city);
+  // app.getLinkToImage(city);
 
   // this block of code when unsplash limit of 50 downloads end
-  let url =
+  const url =
     'https://image.shutterstock.com/z/stock-vector-vector-illustration-in-simple-flat-linear-style-with-smiling-cartoon-characters-teamwork-and-1369217765.jpg';
 
-  app.downloadImage(url, canvas);
+  app.downloadImage(url);
   // this block of code when unsplash limit of 50 downloads end
 
   const currentRes = document.querySelector('.res-active');
@@ -513,4 +491,5 @@ load.addEventListener('click', () => {
   target.classList.add('res-active');
   scale = 1;
   size = 512;
+  isImgLoaded = true;
 });
