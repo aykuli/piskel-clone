1fae1f919a4233d8d18fcd1bf66c6d0a9e21c628
Aykuli <aykuli@ya.ru>
2019-11-24 16:22:43 +0700

    refactor: eslint accordings

--- palette/src/tests/Picture.test.js
+++ palette/src/tests/Picture.test.js
@@ -6,7 +6,7 @@ test('typeof Picture must be function', () => {
 
 test('Picture.getXYCoors(e) must solve right coors from e event', () => {
   const app = new Picture(4, 0, '000000', 0, 0);
-  let e = { offsetX: 100, offsetY: 400 };
+  const e = { offsetX: 100, offsetY: 400 };
 
   expect(app.getXYCoors(e)).toStrictEqual([25, 100]);
 });
