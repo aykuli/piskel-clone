e1b24c0691678b5f3b19181180b3aa62f3dbcc6b
Aykuli <aykuli@ya.ru>
2020-01-20 10:21:51 +0700

    refactor: loginGoogleAccount function

--- simple-piskel-clone/src/components/authentification/firebaseFromGoogle.js
+++ simple-piskel-clone/src/components/authentification/firebaseFromGoogle.js
@@ -3,35 +3,37 @@ import './auth.scss';
 function loginGoogleAccount(firebase, authElements, createPopup, getDomElement) {
   const [authName, authPhoto, authLoginBtn, authLogoutBtn] = authElements;
   const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().useDeviceLanguage();
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then(result => {
-      // The signed-in user info.
-      const { user } = result;
 
-      authPhoto.setAttribute('src', user.providerData[0].photoURL);
-      authName.innerText = user.providerData[0].displayName;
-      authName.parentNode.title = user.email;
-      authLoginBtn.style.display = 'none';
-      authLogoutBtn.style.display = 'block';
-      return result.user;
-    })
-    .catch(e => {
-      createPopup(e.message, getDomElement);
-    });
+  firebase.auth().useDeviceLanguage();
+
+  try {
+    const result = firebase.auth().signInWithPopup(provider);
+    // The signed-in user info.
+    const { user } = result;
+
+    authPhoto.setAttribute('src', user.providerData[0].photoURL);
+    authName.innerText = user.providerData[0].displayName;
+    authName.parentNode.title = user.email;
+    authLoginBtn.style.display = 'none';
+    authLogoutBtn.style.display = 'block';
+
+    return result.user;
+  } catch (e) {
+    createPopup(e.message, getDomElement);
+  }
   return 'authentification passed';
 }
 
 function logoutGoogleAccount(firebase, authElements) {
   const [authName, authPhoto, authLoginBtn, authLogoutBtn] = authElements;
+
   firebase.auth().signOut();
   authPhoto.setAttribute('src', '');
   authName.innerText = '';
 
   authLoginBtn.style.display = 'block';
   authLogoutBtn.style.display = 'none';
+
   return 'user logged out';
 }
 
---
+++ simple-piskel-clone/src/constants.js
@@ -0,0 +1,15 @@
+const LS_KEYS = {
+  pixelSize: 'piskelPixelSize',
+  primaryColor: 'piskelPrimaryColor',
+  secondaryColor: 'piskelSecondaryColor',
+  tool: 'piskelTool',
+  penSize: 'piskelPenSize',
+  fps: 'piskelFps',
+  counter: 'piskelCounter',
+};
+
+const SELECTORS = {
+  FRAME_BUTTON_DELETE: 'frame__btn--delete',
+};
+
+export { LS_KEYS, SELECTORS };
